39dfa7e0c2cd2f8bab2fdfdabfe7125ccb280c2e
S. Andrew Sheppard <andrew@wq.io>
2017-03-21 15:35:30 -0500

    initial commit

---
+++ src/android/TensorFlow.java
@@ -0,0 +1,95 @@
+package io.wq.tensorflow;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import android.util.Base64;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+
+import org.tensorflow.demo.TensorFlowImageClassifier;
+import org.tensorflow.demo.Classifier.Recognition;
+import org.tensorflow.demo.Classifier;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import android.media.ThumbnailUtils;
+
+public class TensorFlow extends CordovaPlugin {
+
+@Override
+public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("loadModel")) {
+        this.loadModel(
+            args.getString(0),
+            args.getString(1),
+            args.getString(2),
+            args.getInt(3),
+            args.getInt(4),
+            (float) args.getDouble(5),
+            args.getString(6),
+            args.getString(7),
+            callbackContext
+        );
+        return true;
+    } else if (action.equals("classify")) {
+        this.classify(args.getString(0), args.getString(1), callbackContext);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+private Map<String,Classifier> classifiers = new HashMap();
+private Map<String,Integer> sizes = new HashMap();
+
+private void loadModel(String modelName, String modelFile, String labelFile,
+                        int inputSize, int imageMean, float imageStd,
+                        String inputName, String outputName,
+                        CallbackContext callbackContext) {
+    classifiers.put(modelName, TensorFlowImageClassifier.create(
+        cordova.getActivity().getAssets(),
+        modelFile,
+        labelFile,
+        inputSize,
+        imageMean,
+        imageStd,
+        inputName,
+        outputName
+    ));
+    sizes.put(modelName, inputSize);
+    callbackContext.success();
+}
+
+private void classify(String modelName, String image, CallbackContext callbackContext) {
+    byte[] imageData = Base64.decode(image, Base64.DEFAULT);
+    Classifier classifier = classifiers.get(modelName);
+    int size = sizes.get(modelName);
+    Bitmap bitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+    Bitmap cropped = ThumbnailUtils.extractThumbnail(bitmap, size, size);
+    List<Recognition> results = classifier.recognizeImage(cropped);
+    JSONArray output = new JSONArray();
+    try {
+        JSONObject info = new JSONObject();
+        info.put("title", "Total");
+        info.put("confidence", results.size());
+        output.put(info);
+        for (Recognition result : results) {
+            JSONObject record = new JSONObject();
+            record.put("title", result.getTitle());
+            record.put("confidence", result.getConfidence());
+            output.put(record);
+        }
+    } catch (JSONException e) {
+    }
+    callbackContext.success(output);
+}
+
+
+}
---
+++ www/tensorflow.js
@@ -0,0 +1,228 @@
+function TensorFlow(modelId, model) {
+    this.modelId = modelId;
+    if (model) {
+        model = registerModel(modelId, model);
+    } else {
+        model = getModel(modelId);
+    }
+    this.model = model;
+    this.onprogress = function() {};
+}
+
+
+TensorFlow.prototype.load = function(successCallback, errorCallback) {
+    var promise;
+    if (window.Promise && !successCallback) {
+        promise = new Promise(function(resolve, reject) {
+            successCallback = resolve;
+            errorCallback = reject;
+        });
+    }
+    loadModel(
+        this.modelId,
+        successCallback,
+        errorCallback,
+        this.onprogress
+    );
+    return promise;
+};
+
+TensorFlow.prototype.classify = function(image, successCallback, errorCallback) {
+    var promise;
+    if (window.Promise && !successCallback) {
+        promise = new Promise(function(resolve, reject) {
+            successCallback = resolve;
+            errorCallback = reject;
+        });
+    }
+
+    var self = this;
+    if (!self.model.loaded) {
+        self.load(function() {
+            if (!self.model.loaded) {
+                errorCallback("Error loading model!");
+                return;
+            }
+            self.classify(image, successCallback, errorCallback);
+        }, errorCallback);
+        return;
+    }
+    
+    cordova.exec(
+        successCallback, errorCallback,
+        "TensorFlow", "classify", [self.modelId, image]
+    );
+    return promise;
+};
+
+// Internal API for downloading and caching model files
+var models = {};
+
+var FIELDS = [
+    'label',
+
+    'model_path',
+    'label_path',
+
+    'input_size',
+    'image_mean',
+    'image_std',
+    'input_name',
+    'output_name'
+];
+
+function registerModel(modelId, model) {
+    FIELDS.forEach(function(field) {
+        if (!model[field]) {
+            throw 'Missing "' + field + '" on model description';
+        }
+    });
+
+    if (model.model_path.match(/^http/) || model.label_path.match(/^http/)) {
+        model.cached = false;
+    } else {
+        model.cached = true;
+    }
+    models[modelId] = model;
+    model.id = modelId;
+    return model;
+}
+
+function getModel(modelId) {
+    var model = models[modelId];
+    if (!model) {
+        errorCallback("Unknown model " + modelId);
+        return;
+    }
+    return model;
+}
+
+var INCEPTION = 'https://storage.googleapis.com/download.tensorflow.org/models/';
+registerModel('inception-v1', {
+    'label': 'Inception v1',
+    'model_path': INCEPTION + 'inception5h.zip#tensorflow_inception_graph.pb',
+    'label_path': INCEPTION + 'inception5h.zip#imagenet_comp_graph_label_strings.txt',
+    'input_size': 224,
+    'image_mean': 117,
+    'image_std': 1,
+    'input_name': 'input',
+    'output_name': 'output'
+});
+
+registerModel('inception-v3', {
+    'label': 'Inception v3',
+    'model_path': INCEPTION + 'inception_dec_2015.zip#tensorflow_inception_graph.pb',
+    'label_path': INCEPTION + 'inception_dec_2015.zip#imagenet_comp_graph_label_strings.txt',
+    'input_size': 299,
+    'image_mean': 128,
+    'image_std': 128,
+    'input_name': 'Mul',
+    'output_name': 'final_result'
+});
+
+function loadModel(modelId, callback, errorCallback, progressCallback) {
+    var model = getModel(modelId);
+    if (!progressCallback) {
+        progressCallback = function(stat) {
+            console.log(stat.label);
+        };
+    }
+    if (!model.cached) {
+        fetchModel(model, initClassifier, errorCallback, progressCallback);
+    } else {
+        initClassifier();
+    }
+    function initClassifier() {
+        var modelPath = (model.local_model_path || model.model_path),
+            labelPath = (model.local_label_path || model.label_path);
+        modelPath = modelPath.replace(/^file:\/\//, '');
+        labelPath = labelPath.replace(/^file:\/\//, '');
+        progressCallback({
+            'status': 'initializing',
+            'label': 'Initializing classifier'
+        });
+        cordova.exec(function() {
+            model.loaded = true;
+            callback(model);
+        }, errorCallback, "TensorFlow", "loadModel", [
+            model.id,
+            modelPath,
+            labelPath,
+            model.input_size,
+            model.image_mean,
+            model.image_std,
+            model.input_name,
+            model.output_name
+        ]);
+    }
+}
+
+function getPath(filename) {
+    return (
+        cordova.file.externalDataDirectory || cordova.file.dataDirectory
+    ) + filename;
+}
+
+function fetchModel(model, callback, errorCallback, progressCallback) {
+    fetchZip(model, callback, errorCallback, progressCallback);
+}
+
+function fetchZip(model, callback, errorCallback, progressCallback) {
+    var zipUrl = model.model_path.split('#')[0];
+    if (model.label_path.indexOf(zipUrl) == -1) {
+        errorCallback('Model and labels must be in same zip file!');
+        return;
+    }
+    var modelZipName = model.model_path.replace(zipUrl + '#', '');
+    var labelZipName = model.label_path.replace(zipUrl + '#', '');
+    var zipPath = getPath(model.id + '.zip');
+    var dir = getPath(model.id);
+
+    model.local_model_path = dir + '/' + modelZipName;
+    model.local_label_path = dir + '/' + labelZipName;
+
+    resolveLocalFileSystemURL(model.local_model_path, alreadyLoaded, downloadZip);
+
+    function alreadyLoaded() {
+        model.downloaded = true;
+        callback();
+    }
+
+    function downloadZip() {
+        var fileTransfer = new FileTransfer();
+        progressCallback({
+            'status': 'downloading',
+            'label': 'Downloading model files',
+        });
+        fileTransfer.onprogress = function(evt) {
+            var label = 'Downloading';
+            if (evt.lengthComputable) {
+                label += ' (' + evt.loaded + '/' + evt.total + ')';
+            } else {
+                label += '...';
+            }
+            progressCallback({
+                'status': 'downloading',
+                'label': label,
+                'detail': evt
+            });
+        };
+        fileTransfer.download(zipUrl, zipPath, function(entry) {
+            progressCallback({
+                'status': 'unzipping',
+                'label': 'Extracting contents'
+            });
+            zip.unzip(zipPath, dir, function(result) {
+                if (result == -1) {
+                    errorCallback('Error unzipping file');
+                    return;
+                }
+                model.downloaded = true;
+                callback();
+            });
+        }, errorCallback);
+    }
+}
+
+TensorFlow._models = models;
+module.exports = TensorFlow;
